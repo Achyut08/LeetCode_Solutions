9643fe8a59f5c29684da19cb0412981741f3b94c
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2021-11-15 21:16:48 +0530

    Create 73. Set Matrix Zeroes.py

---
+++ Interview questions of leetcode/73. Set Matrix Zeroes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        row0=len(matrix)*[False]
+        col0=len(matrix[0])*[False]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==0:
+                    
+                    row0[i]= True
+                    col0[j]= True
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if row0[i] or col0[j]:
+                    matrix[i][j]=0
+            
+        
+        
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
