cb04426d5e0d2a6006c34c381ecf301b626f8e48
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-02-20 15:13:25 +0530

    Time: 61 ms (13.03%), Space: 13.8 MB (99.31%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,9 +1,25 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1 = 0
-        rob2 = 0
-        for i in nums:
-            temp = max(rob1+i,rob2)
-            rob1=rob2
-            rob2=temp
-        return rob2
+        length = len(nums)
+        if length==0:
+            return 0
+        if length==1:
+            return nums[0]
+        if length==2:
+            return max(nums)
+			
+        dp = [0]*length # assign dp array
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        
+        for i in range(2, length):
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+        print(dp)
+        
+        return max(dp)
+        # rob1 = 0
+        # rob2 = 0
+        # for i in nums:
+        #     temp = max(rob1+i,rob2)
+        #     rob1=rob2
+        #     rob2=temp
+        # return rob2
