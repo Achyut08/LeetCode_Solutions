d442ed432a423e204f87501268f8d5af382a5dc9
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-02-22 18:51:05 +0530

    Time: 1266 ms (15.93%), Space: 25.1 MB (28.66%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,18 +1,35 @@
 class Solution(object):
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+#         m = len(text1)
+#         n = len(text2)
+#         dp = [[-1 for i in range(n+1)] for i in range(m+1)]
+		
+#         def helper(text1,text2,m,n):
+#             if m == 0 or n == 0:
+#                 return 0
+#             if dp[m][n] != -1:
+#                 return dp[m][n]
+#             if text1[m-1] == text2[n-1]:
+#                 dp[m][n] = 1 + helper(text1,text2,m-1,n-1)
+#             else:
+#                 dp[m][n] = max(helper(text1,text2,m-1,n),helper(text1,text2,m,n-1))
+#             return dp[m][n]
+#         return helper(text1,text2,m,n)
+			
+        
+        
         m = len(text1)
         n = len(text2)
-        dp = [[-1 for i in range(n+1)] for i in range(m+1)]
-		
-        def helper(text1,text2,m,n):
-            if m == 0 or n == 0:
+        dp = [[-1 for i in range(n+1)] for j in range(m+1)]
+        def solve(text1,text2,m,n):
+            if n == 0 or m == 0:
                 return 0
             if dp[m][n] != -1:
                 return dp[m][n]
             if text1[m-1] == text2[n-1]:
-                dp[m][n] = 1 + helper(text1,text2,m-1,n-1)
+                dp[m][n] = 1 + solve(text1,text2,m-1,n-1)
             else:
-                dp[m][n] = max(helper(text1,text2,m-1,n),helper(text1,text2,m,n-1))
+                dp[m][n] = max(solve(text1,text2,m,n-1),solve(text1,text2,m-1,n))
             return dp[m][n]
-        return helper(text1,text2,m,n)
+        return solve(text1,text2,m,n)
 			
