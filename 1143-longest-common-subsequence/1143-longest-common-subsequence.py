2a96478184cdde4bed43d660b8498d334a2e4b37
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-02-24 17:50:22 +0530

    Time: 612 ms (50.07%), Space: 29.8 MB (26.07%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,5 +1,15 @@
 class Solution(object):
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+    def longestCommonSubsequence(self, A: str, B: str) -> int:
+        n, m = len(A), len(B)
+        dp = [["" for _ in range(m + 1)] for _ in range(n + 1)]
+        for i in range(n):
+            for j in range(m):
+                if A[i] == B[j]:
+                    dp[i + 1][j + 1] = dp[i][j] + A[i]
+                else:
+                    dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1], key=len)
+        #print(dp)
+        return (len(dp[-1][-1]))
 #         m = len(text1)
 #         n = len(text2)
 #         dp = [[-1 for i in range(n+1)] for i in range(m+1)]
@@ -18,18 +28,21 @@ class Solution(object):
 			
         
         
-        m = len(text1)
-        n = len(text2)
-        dp = [[-1 for i in range(n+1)] for j in range(m+1)]
-        def solve(text1,text2,m,n):
-            if n == 0 or m == 0:
-                return 0
-            if dp[m][n] != -1:
-                return dp[m][n]
-            if text1[m-1] == text2[n-1]:
-                dp[m][n] = 1 + solve(text1,text2,m-1,n-1)
-            else:
-                dp[m][n] = max(solve(text1,text2,m,n-1),solve(text1,text2,m-1,n))
-            return dp[m][n]
-        return solve(text1,text2,m,n)
+#         m = len(text1)
+#         n = len(text2)
+#         dp = [[-1 for i in range(n+1)] for j in range(m+1)]
+#         def solve(text1,text2,m,n):
+#             if n == 0 or m == 0:
+#                 return 0
+#             if dp[m][n] != -1:
+#                 return dp[m][n]
+#             if text1[m-1] == text2[n-1]:
+#                 dp[m][n] = 1 + solve(text1,text2,m-1,n-1)
+#             else:
+#                 dp[m][n] = max(solve(text1,text2,m,n-1),solve(text1,text2,m-1,n))
+#             return dp[m][n]
+#         return solve(text1,text2,m,n)
+
+
+
 			
