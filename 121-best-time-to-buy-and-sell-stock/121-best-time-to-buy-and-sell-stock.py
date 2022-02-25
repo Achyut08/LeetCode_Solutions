ff9ecb6910a59243b498cf1a9291cc065e9aad0e
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-02-25 21:58:54 +0530

    Time: 1418 ms (51.64%), Space: 25.5 MB (7.66%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,31 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        lst = float(inf) #least so far
+        profit = 0
+        psit = 0 #profit if soild today
+        lst = prices[0]
+        for i in range(1,len(prices)):
+            if (prices[i] < lst):
+                lst = prices[i]
+                print(lst)
+            psit = prices[i] - lst
+            print(psit)
+            if (psit > profit):
+                profit = psit
+            # print(lst, profit)
+        return profit
         
-        if len(prices) == 0:
-            return 0
-        else:
-            max_profit = 0
-            min_price = prices[0]
-            for i in range(len(prices)):
-                profit = prices[i] - min_price   # yaha bech(sell) kar rahe hai
-                max_profit = max(profit, max_profit)
-                min_price = min(min_price, prices[i])
+#         if len(prices) == 0:
+#             return 0
+#         else:
+#             max_profit = 0
+#             min_price = prices[0]
+#             for i in range(len(prices)):
+#                 profit = prices[i] - min_price   # yaha bech(sell) kar rahe hai
+#                 max_profit = max(profit, max_profit)
+#                 min_price = min(min_price, prices[i])
 
-            return max_profit
+#             return max_profit
        
         
         
