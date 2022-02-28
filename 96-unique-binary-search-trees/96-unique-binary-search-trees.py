515016ad1639f3aab3d56c9141ff96a02ca64448
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-02-28 09:46:05 +0530

    Time: 85 ms (5.36%), Space: 14.3 MB (25.27%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        catalan = []
+        catalan.append(1)
+        for i in range(1,10001):
+                       x = catalan[i-1]*(4*i-2)/(i+1)
+                       catalan.append(x)
+        return int(catalan[n])  
