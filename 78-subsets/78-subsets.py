91bd2c6d61fea45cb6b9983a03d3a33b3e898040
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-03-03 19:41:38 +0530

    Time: 38 ms (73.97%), Space: 14.1 MB (54.63%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,24 @@
+class Solution:
+    
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        #recursive
+        res = []
+        self.backtracking(res,0,[],nums)
+        return res
+    def backtracking(self,res,start,subset,nums):
+        res.append(list(subset))
+        for i in range(start,len(nums)):
+            subset.append(nums[i])
+            self.backtracking(res,i+1,subset,nums)
+            subset.pop()   #push_back
+        # #itrative
+        # if len(nums) == 0:
+        #     return [[]]
+        # result = [[]]
+        # for x in nums:
+        #     n = len(result)
+        #     for i in range(n):
+        #         r = result[i] + [x]
+        #         result.append(r)
+        # return result
