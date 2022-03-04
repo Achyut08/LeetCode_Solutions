6f2464627699b733be7d0de2c3876326ea8b7df7
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-03-04 10:10:13 +0530

    Time: 49 ms (63.12%), Space: 14.2 MB (72.35%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -11,21 +11,24 @@ class Solution:
                 continue
             subset.append(nums[i])
             self.backtracking(res,i+1,subset,nums)
-            subset.pop()   #push_back
-            
-            
-            
-            
-            
-#               nums = sorted(nums)
+            subset.pop()
+
+    
+        
+        
+#         nums.sort()
 #         res = []
 #         self.backtracking(res,0,[],nums)
 #         return res
 #     def backtracking(self,res,start,subset,nums):
 #         res.append(list(subset))
 #         for i in range(start,len(nums)):
-#             if i > start and nums[i] == nums[i-1]:
+#             if (i != start and nums[i] == nums[i-1]):
 #                 continue
 #             subset.append(nums[i])
 #             self.backtracking(res,i+1,subset,nums)
-#             subset.pop()
+#             subset.pop()   #push_back
+            
+            
+            
+          
