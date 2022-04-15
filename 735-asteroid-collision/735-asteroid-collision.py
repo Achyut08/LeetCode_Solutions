bdfb9fd84d3fc916ac5ed83573cc1c1de63053af
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-04-16 00:00:14 +0530

    Time: 100 ms (89.71%), Space: 15.2 MB (76.85%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.py
@@ -0,0 +1,34 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+            
+        # positive means moving right, negative means moving left
+        stack = []
+        for asteroid in asteroids:
+            # safe to enter stack:
+            # 1. when stack is empty
+            # 2. when asteroid and stack[-1] both moving left (negative)
+            # 3. when asteroid moving right (positive) (regardless of stack[-1])
+            if len(stack) == 0 or asteroid * stack[-1] > 0 or asteroid > 0:
+                stack.append(asteroid)
+            # possible collision: asteroid moving left, stack[-1] moving right
+            if asteroid < 0 and stack[-1] > 0:
+                # keep popping stack
+                while stack and abs(asteroid) > abs(stack[-1]) and stack[-1] > 0:
+                    stack.pop()
+                # cancel out
+                if stack and asteroid + (stack[-1]) == 0:
+                    stack.pop()
+                # empty stack or same direction
+                elif len(stack) == 0 or asteroid * stack[-1] > 0:
+                    stack.append(asteroid)
+        return stack
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
