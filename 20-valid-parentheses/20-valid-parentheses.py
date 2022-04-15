2e01c21d5d8d453c42bb877a331c149f7f8a2f86
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-04-15 22:39:22 +0530

    Time: 27 ms (96.17%), Space: 13.8 MB (74.40%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,17 +1,31 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) % 2 != 0:
-            return False
-        dic = {'(' : ')', '[' : ']', '{' : '}'}
+          # Initialize and empty stack
         stack = []
-        g = dic.keys()
-        for c in s:
-            if c in dic.keys():
-                stack.append(c)
+        
+        # loop over the string
+        for char in s:
+            
+            # if we find any opening bracket then we push it on the stack
+            if char == '(' or char == '[' or char == '{':
+                stack.append(char)
+                
+            # This statement only gets triggered when the string starts with closing bracket eg: },),]
+            elif not stack:
+                return False
+            
+            # if we find any closing bracket after pushing to the stack we check if it's equal to the last pushed char
+            # if it is we pop it off the stack if not then we return False
+            elif char == ')' and stack[-1] == '(':
+                stack.pop()
+            elif char == ']' and stack[-1] == '[':
+                stack.pop()
+            elif char == '}' and stack[-1] == '{':
+                stack.pop()
             else:
-                if stack == []:
-                    return False
-                a = stack.pop()
-                if c != dic[a]:
-                    return False
-        return stack == []
+                return False
+            
+            
+        # in the end if all closing brackets are found then stack will be empty
+        return len(stack) == 0
+     
