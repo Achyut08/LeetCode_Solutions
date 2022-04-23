e381d70561d611cdc114620225d2cd9eca644da8
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-04-23 15:09:40 +0530

    Time: 45 ms (53.57%), Space: 13.9 MB (28.27%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.py
@@ -0,0 +1,18 @@
+class Codec:
+    def __init__(self):
+        self.map = dict()
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        key = hash(longUrl)
+        self.map[key] = longUrl
+        return f"http://tinyurl.com/{key}"
+        
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        parts = shortUrl.split('/')
+        key = parts[-1]
+        return self.map[int(key)]
