d20fc0d273abfe7da6260f6c2603f34551812b01
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-05-07 12:01:37 +0530

    Time: 112 ms (14.71%), Space: 14 MB (48.30%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,9 +1,6 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-         # Loop through every cost after the first two steps
-        for i in range(2, len(cost)):
-            # Update the cheapest cost to step here
-            cost[i] += min(cost[i-2], cost[i-1])
-        
-        # Cheapest cost of the last two steps is the answer
-        return min(cost[len(cost)-2], cost[len(cost)-1])
+        for i in range(2,len(cost)):
+            cost[i] += min(cost[i-2],cost[i-1])
+        print(cost)
+        return min(cost[-1],cost[-2])
