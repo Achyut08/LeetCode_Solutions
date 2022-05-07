7ee257f9cbe7765a999e24ef0d52e05d10dbbf89
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-05-07 11:36:06 +0530

    Time: 45 ms (38.61%), Space: 13.8 MB (96.62%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -6,37 +6,3 @@ class Solution:
         for i in range(2,n+1):
             dp[i] = dp[i-1] +dp[i-2]
         return dp[n]
-        
-        
-        
-        
-#         def solve(n):
-#             if dp[n] != -1:
-#                 return dp[n]
-#             if n == 0:
-#                 dp[n] = 1
-#             elif n == 1:
-#                 dp[n] = 1
-#             else:
-#                 dp[n] = solve(n-1) + solve(n-2)
-#             return dp[n]
-#         dp = [-1] * (n+1)
-#         return solve(n)
-    
-    
-        
-        
-        
-#         self.d = {}
-#         return self.climbstair(n)
-#     def climbstair(self,n):
-#         if n==1:
-#             return 1
-#         if n==0:
-#             return 1
-#         if (n) in self.d:
-#             return self.d[n]
-#         else:
-#             self.d[n]= self.climbstair(n-1)+self.climbstair(n-2)
-#         return self.d[n]
-    
