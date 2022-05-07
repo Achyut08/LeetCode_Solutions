0ee29cd7515edc45fbd3c98bf6ad93200b454973
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-05-07 11:48:03 +0530

    Time: 44 ms (56.75%), Space: 13.9 MB (56.54%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -2,36 +2,12 @@ class Solution:
     def fib(self, n: int) -> int:
         if n == 0:
             return 0
-        if n == 1 or n == 2:
+        if n==1 or n==2 :
             return 1
-        bottom_up = [0]*(n+1)
-        bottom_up[1] = 1
-        bottom_up[2] = 1
-        for i in range(3, n+1):
-            bottom_up[i] = bottom_up[i-1] + bottom_up[i-2]
-        return bottom_up[n]
-        # def solve(n):
-        #     if dp[n] != -1:
-        #         return dp[n]
-        #     if n == 0:
-        #         dp[n] = 0
-        #     elif n == 1:
-        #         dp[n] = 1
-        #     else:
-        #         dp[n] = solve(n-1) + solve(n-2)
-        #     return dp[n]
-        # dp = [-1] * (n+1)
-        # return solve(n)
-    
-    
-    
-#     class Solution:
-#     def fib(self, N: int) -> int:      
-#         def dp(n):
-#             if memo[n] != None:
-#                 return memo[n]
-#             memo[n] = 0 if n==0 else 1 if n == 1 else dp(n-1) + dp(n-2)
-#             return memo[n]  
-        
-#         memo = [None] * (N+1)
-#         return dp(N)
+        dp = [-1] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
