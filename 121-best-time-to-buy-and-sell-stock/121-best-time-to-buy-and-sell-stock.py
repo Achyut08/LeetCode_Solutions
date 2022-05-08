916da82111733e5b1212d65331b6be7712e740a9
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-05-08 10:25:22 +0530

    Time: 1639 ms (23.83%), Space: 25.1 MB (39.82%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,31 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        lst = float(inf) #least so far
-        profit = 0
-        psit = 0 #profit if soild today
-        lst = prices[0]
-        for i in range(1,len(prices)):
-            if (prices[i] < lst):
-                lst = prices[i]
-                print(lst)
-            psit = prices[i] - lst
-            print(psit)
-            if (psit > profit):
-                profit = psit
-            # print(lst, profit)
-        return profit
+        #set max profit to 0
+        max_profit=0
+        #set min price to the first price in the prices array
+        min_price=prices[0]
         
-#         if len(prices) == 0:
-#             return 0
-#         else:
-#             max_profit = 0
-#             min_price = prices[0]
-#             for i in range(len(prices)):
-#                 profit = prices[i] - min_price   # yaha bech(sell) kar rahe hai
-#                 max_profit = max(profit, max_profit)
-#                 min_price = min(min_price, prices[i])
-
-#             return max_profit
-       
-        
-        
+        #loop from the second price in the array since first price is captured in min_price
+        for i in range(1,len(prices)): 
+            #update min_price
+            min_price=min(min_price, prices[i])
+            #updte max profit
+            max_profit=max(max_profit,prices[i]-min_price)  
+        return max_profit
