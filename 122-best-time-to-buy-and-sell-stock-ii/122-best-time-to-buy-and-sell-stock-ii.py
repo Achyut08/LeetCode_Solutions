1a78d2cd38b1f699ffad63f0e9c20bade24cf363
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-05-08 10:30:29 +0530

    Time: 110 ms (21.40%), Space: 15.1 MB (69.33%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,7 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        
+        #This is the max total profit we can make from the prices array
         profit = 0
-        for i in range(1,len(prices)):
-            if prices[i] > prices[i-1]:
-                profit += prices[i] - prices[i-1]
+        
+        #We iterate through the prices array starting at 1
+        for i in range(1, len(prices)):
+            #If the current price is larger than the previous value, that mean's we're going
+            #to sell it
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+        
+        #We return the profit
         return profit
