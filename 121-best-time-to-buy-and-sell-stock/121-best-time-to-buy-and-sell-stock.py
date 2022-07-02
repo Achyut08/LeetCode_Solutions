6db47f6f54fcd92b18e23abd2f01f2587ea4347c
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-07-02 11:06:16 +0530

    Time: 2090 ms (10.94%), Space: 25.1 MB (6.55%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #set max profit to 0
-        max_profit=0
-        #set min price to the first price in the prices array
-        min_price=prices[0]
-        
-        #loop from the second price in the array since first price is captured in min_price
-        for i in range(1,len(prices)): 
-            #update min_price
-            min_price=min(min_price, prices[i])
-            #updte max profit
-            max_profit=max(max_profit,prices[i]-min_price)  
+        max_profit = 0
+        min_price = prices[0]
+        for i in range(1,len(prices)):
+            min_price = min(min_price,prices[i])
+            max_profit = max(max_profit,prices[i]-min_price)
         return max_profit
