fb4c962f1b8f7af72bc63551abf1a46baa761475
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-08-25 19:10:27 +0530

    Time: 977 ms (68.12%), Space: 28 MB (40.00%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,12 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        #kadan's algorithm
-        sums=0
-        maximun=nums[0]
-        for i in nums:
-            if sums<0:
-                sums=0
-            sums += i
-            maximun=max(sums,maximun)
-        return maximun
-        
+        sum1 = 0
+        maxi = nums[0]
+        for i in range(len(nums)):
+            if sum1 < 0:
+                sum1 = 0
+            sum1 += nums[i]
+            maxi = max(maxi,sum1)
+        return maxi
