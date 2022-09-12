7304c3f518d71d08def8511e47b6892c6142417c
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2022-09-12 09:35:55 +0530

    Time: 82 ms (21.94%), Space: 14 MB (25.18%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -1,38 +1,53 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        n=len(nums)
-        k=n-2
-        l=0
-        while k>=0:
-            if nums[k]<nums[k+1]:
+        idx = -1
+        for i in range(len(nums)-2,-1,-1):
+            if nums[i] < nums[i+1]:
+                idx = i
                 break
-            k -= 1
-            
-        if k<0:
-            s = 0 
-            e= n-1 
-            while s<=e:
-                nums[s],nums[e] = nums[e],nums[s]
-                s+=1 
-                e-=1
-            return nums
-        else:
-            l=n-1
-            while l>k:
-                if nums[l]>nums[k]:
+        if idx > -1:
+            for i in range(len(nums)-1,-1,-1):
+                if nums[i] > nums[idx]:
+                    idx2 = i
                     break
-                l -= 1
-            nums[k],nums[l]=nums[l],nums[k]
-            s = k+1 
-            e = n-1 
-            while s<=e:
-                nums[s],nums[e] = nums[e],nums[s]
-                s+=1 
-                e-=1 
-        return nums
-     
-        
+            nums[idx],nums[idx2] = nums[idx2],nums[idx]
+            l = idx+1
+            r = len(nums)-1
+            while (l<r):
+                nums[l],nums[r] = nums[r],nums[l]
+                l+= 1
+                r -= 1
+        else:
+            nums.reverse()
 
-
-
-        
+    
+    
+    
+    
+#     Find the index of the first element which satisfies nums[i] < nums[i+1] : ind1
+# Find the index of the first element which satisfies the condition nums[i] > nums[ind1] : ind2
+# Swap nums[ind1] and nums[ind2]
+# From the index ind1 to len(nums)-1 reverse the list
+# If ind1 is not found, just reverse the list
+# def nextPermutation(self, nums: List[int]) -> None:
+#     """
+#     Do not return anything, modify nums in-place instead.
+#     """
+#     ind1 = -1
+#     for i in range(len(nums)-2,-1,-1):
+#         if nums[i] < nums[i+1]:
+#             ind1 = i
+#             break
+#     if ind1 > -1:
+#         for i in range(len(nums)-1,-1,-1):
+#             if nums[i]>nums[ind1]:
+#                 ind2 = i
+#                 break
+#         nums[ind1],nums[ind2] = nums[ind2],nums[ind1]
+#         l,r = ind1+1,len(nums)-1
+#         while(l<r):
+#             nums[l],nums[r] = nums[r],nums[l]
+#             l+=1
+#             r-=1
+#     else:
+#         nums.reverse()
